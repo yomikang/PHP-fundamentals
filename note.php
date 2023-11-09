fc26eab9e64d3bef7894a19181a41815d672373d
yomikang <49647393+yomikang@users.noreply.github.com>
2023-11-09 20:54:18 +0000

    note file added

---
+++ note.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+-How to leave comment in PHP
+1. This is single-line comment
+2. This is Multiline comment
+3. # another single-line comment
+
+-variable 
+1. Camel Casing: $thisIsMyAge;
+2. Pascal Casing: $ThisIsMyAge;
+3. Snake Casing: $this_is_my_age;
+
+-data type check
+var_dump(variable that you want to check its data type)
+
+-use underscore as a , for large numbers
+*/
+
