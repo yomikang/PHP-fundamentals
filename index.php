8a49390a9a102782e6d76127fc14e35b11861811
yomikang <49647393+yomikang@users.noreply.github.com>
2023-11-09 20:57:39 +0000

    string

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+<?php 
 
-  <!--
-  This script places a badge on your repl's full-browser view back to your repl's cover
-  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
-  teal, blue, blurple, magenta, pink!
-  -->
-  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
-  </body>
-</html>
+
+$data1 = 'John $data3';
+$data3 = "What about this?";
+$data2 = "$data1 smith $data3";
+
+var_dump($data2);
+var_dump($data1);
