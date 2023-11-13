82b908886c28d76dde46b821756f5a36dfdd89bb
yomikang <49647393+yomikang@users.noreply.github.com>
2023-11-13 16:08:39 +0000

    Typecasting

--- note.php
+++ note.php
@@ -1,6 +1,12 @@
 <?php
 
 /* 
+
+-PHP and HTML
+1. HTML 안에 PHP들어가는거 가능
+2. PHP안에 HTML 들어가는거 불가능
+3. PHP 코드는 <?php ?> 로 시작해야함
+
 -How to leave comment in PHP
 1. This is single-line comment
 2. This is Multiline comment
@@ -18,6 +24,12 @@ var_dump(variable that you want to check its data type)
 
 -string 
 If you want to have a variable within a string, string quotation mark should be always double not single
+
+-Typecasting is the process of changing the data type of an existing value into a different data type. Typecasting is done automatically by PHP, but it can also be done manually.
+
+
+
+
 
 */
 
